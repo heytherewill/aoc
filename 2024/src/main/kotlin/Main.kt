da9e3377d151a84ec72a7ee341bcf2681b6bf7c5
William Barbosa <heytherewill@gmail.com>
2024-12-02 07:01:00 +0100

    ☢️ Day 2

--- 2024/src/main/kotlin/Main.kt
+++ 2024/src/main/kotlin/Main.kt
@@ -1,28 +1,47 @@
 package com.heytherewill
 
 import com.heytherewill.solutions.HistorianHysteria
+import com.heytherewill.solutions.RedNosedReports
 
-val solutions = listOf(
-    HistorianHysteria()
+private val solutions = listOf(
+    HistorianHysteria(),
+    RedNosedReports()
 )
+
+private const val lineInternalSpace = 31
+private val separationText = List(lineInternalSpace) { "-" }.joinToString("")
+
+private fun printLineWithPadding(text: String) {
+    val totalPadding = lineInternalSpace - text.length
+    val halfPadding = totalPadding / 2
+    val paddingLeft = List(halfPadding) { " " }.joinToString("")
+    val paddingRight = List(totalPadding - halfPadding) { " " }.joinToString("")
+
+    print("|${paddingLeft}${text}${paddingRight}|\n")
+}
+
+private fun printTreeSeparator() {
+    print("|${separationText}|\n")
+}
 
 fun main() {
 
-    print("                -                \n");
-    print("               ---               \n");
-    print("             --*----             \n");
-    print("           -------*---           \n");
-    print("         ------*---*----         \n");
-    print("       ---O----------O----       \n");
-    print("     -------*---O-----*-----     \n");
-    print("   ----O-----*------*---------   \n");
-    print(" ---------O------*-----------*-- \n");
-    print("---------------------------------\n");
-    print("|     Advent of Code   2024     |\n");
-    print("|-------------------------------|\n");
+    print("                -                \n")
+    print("               ---               \n")
+    print("             --*----             \n")
+    print("           -------*---           \n")
+    print("         ------*---*----         \n")
+    print("       ---O----------O----       \n")
+    print("     -------*---O-----*-----     \n")
+    print("   ----O-----*------*---------   \n")
+    print(" ---------O------*-----------*-- \n")
+    print("---------------------------------\n")
+    printLineWithPadding("Advent of Code 2024")
+    printTreeSeparator()
     for (solution in solutions) {
-        print("|Problem ${solution.day} (${solution.name}) |\n");
-        print("|-------------------------------|\n");
-        print("| I: ${solution.solvePartOne()}   |  II: ${solution.solvePartTwo()}  |\n")
+        printLineWithPadding("Day ${solution.day}: ${solution.name}")
+        printTreeSeparator()
+        printLineWithPadding("I: ${solution.solvePartOne()} | II: ${solution.solvePartTwo()}")
+        printTreeSeparator()
     }
 }
---
+++ 2024/src/main/kotlin/solutions/02RedNosedReports.kt
@@ -0,0 +1,63 @@
+package com.heytherewill.solutions
+
+import com.heytherewill.utilities.readFirstInputForDay
+import com.heytherewill.utilities.readSecondInputForDay
+import kotlin.math.abs
+
+class RedNosedReports : Solution {
+    override val day = 2
+    override val name = "Red-Nosed Reports"
+    private val safetyThreshold = 3
+
+    override fun solvePartOne(): Long {
+        val input = readFirstInputForDay(2)
+        return solve(input, problemDampenerIsActive = false)
+    }
+
+    override fun solvePartTwo(): Long {
+        val input = readSecondInputForDay(2)
+        return solve(input, problemDampenerIsActive = true)
+    }
+
+    private fun solve(input: String, problemDampenerIsActive: Boolean): Long {
+        val reports = parseAsReports(input)
+        return reports.count { report ->
+            val firstViolatingIndex = findFirstViolatingIndex(report) ?: return@count true
+
+            if (problemDampenerIsActive) {
+                val problemIsFixable =
+                    // Check for the first and second items and the items around the problematic level.
+                    findFirstViolatingIndex(report.indices.drop(1).map { report[it] }) == null ||
+                    findFirstViolatingIndex(report.indices.filter { it != 1 }.map { report[it] }) == null ||
+                    findFirstViolatingIndex(report.indices.filter { it != firstViolatingIndex - 1 }.map { report[it] }) == null ||
+                    findFirstViolatingIndex(report.indices.filter { it != firstViolatingIndex }.map { report[it] }) == null ||
+                    findFirstViolatingIndex(report.indices.filter { it != firstViolatingIndex + 1 }.map { report[it] }) == null
+                if (problemIsFixable) {
+                    return@count true
+                }
+            }
+
+            return@count false
+        }.toLong()
+    }
+
+    private fun findFirstViolatingIndex(report: List<Long>): Int? {
+        val trend = report[0] < report[1]
+        for (levelIndex in report.indices.drop(1)) {
+            val previous = report[levelIndex - 1]
+            val current = report[levelIndex]
+            val isIncreasing = previous < current
+            val difference = abs(current - previous)
+            if (trend != isIncreasing || difference < 1 || difference > safetyThreshold) {
+                return levelIndex
+            }
+        }
+
+        return null
+    }
+
+    private fun parseAsReports(input: String) : List<List<Long>> =
+        input.lines().map { line ->
+            line.split(" ").map { it.toLong() }
+        }
+}
